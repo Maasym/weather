fffbe1c61b72cda305929997b3c857cc12df278a
maasym <github1@go-nihon.com>
2022-04-03 15:18:05 +0200

    init and design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+
 
 function App() {
+  //const url = 'http://api.weatherapi.com/v1/forecast.json?key=adeca4f1a42a4bf0a69115018220304&q=dallas&days=1&aqi=no&alerts=no'
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+            <div className="location">
+                <p>Dallas</p>
+            </div>
+            <div className="temp">
+                <h1>60° C</h1>
+            </div>
+            <div className="description">
+                <p>Clouds</p>
+            </div>
+        </div>
+        <div className='bottom'>
+            <div className="feels">
+                <p>65° C</p>
+            </div>
+            <div className="humidity">
+                <p>20%</p>
+            </div>
+            <div className="wind">
+                <p>12 KMH</p>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
